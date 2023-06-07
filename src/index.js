aeb657067c724c96631b7b8136cc162ead392297
Pallavi Singh <78789162+Pallavi-s-r@users.noreply.github.com>
2023-06-07 14:53:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://chaudharyaditya41:Z67gI1uJnrGCnHuY@cluster0.jgngtnq.mongodb.net/Group10Database1?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://pallavisinghsgrl9:6mILIfTCfLk8gUxt@cluster0.zkm1tbf.mongodb.net/urlDatabase'retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
 .then(() => console.log("MongoDB is connected"))
